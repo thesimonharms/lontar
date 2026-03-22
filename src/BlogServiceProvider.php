18bf864f3457b435b558874962f380c3815c3f3b
Simon Harms <simon@simonharms.com>
2026-03-22 16:11:53 -0400

    Fixed route bug where applications using lontar weren't having the /api prefix.

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -2,13 +2,14 @@
 
 namespace Lontar\Blog;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class BlogServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        Route::middleware('api')->prefix('api')->group(__DIR__ . '/../routes/api.php');
 
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
